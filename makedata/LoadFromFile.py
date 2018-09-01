ffee30360afa515ea6d1aa85dad56b695141965d
Matt Carey <mscarey@gmail.com>
2018-09-01 18:46:35 -0500

    regular expression filtering, about to replace with list lookup

--- makedata/LoadFromFile.py
+++ makedata/LoadFromFile.py
@@ -1,4 +1,5 @@
 import csv
+import os
 import re
 
 def load_region_file(apple_path: str) -> list:
@@ -10,7 +11,10 @@ def load_region_file(apple_path: str) -> list:
 
 def get_year(year: int) -> list:
     # year_col = "YR{}".format(str(year)[-2:])
-    apple_path = './data/from_agency/by_region/REGION_{}_DISTRICT_summary_{}.csv'
+    dirname=os.path.dirname
+    apple_path = os.path.join(dirname(dirname(__file__)), 
+                              os.path.join('data', 'from_agency', 'by_region',
+                              'REGION_{}_DISTRICT_summary_{}.csv'))
     one_year = [load_region_file(apple_path.format(str(region).zfill(2),str(year)[-2:]))
                 for region in range(1,21)]
     return [item for sublist in one_year for item in sublist]
@@ -21,9 +25,9 @@ def filter_year_by_column(year_of_records: list,
                           reject_matches=False) -> list:
 
     i = year_of_records[0].index(column_name)
-
+    pattern = re.compile(pattern)
     year_of_records[1:] = [row for row in year_of_records[1:] 
-                           if re.match(row[i], pattern) != reject_matches]
+                           if re.search(pattern, row[i]) != reject_matches]
     return year_of_records
     
 
