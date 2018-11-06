401084edffa3c6bc1f7c567e1bcbe8114ae37208
Rebecca Frank <rebeccaffrank@gmail.com>
2018-11-06 08:41:54 -0600

    Update tests for new data format
    - Add a new mock element in the dom for the year selector
    - Modify mock data to reflect the new data format

--- js/index.test.js
+++ js/index.test.js
@@ -1,47 +1,24 @@
 const mockData = {
-    2015: {
-        ALL: {
-            POP: {
-                1902: { C: 640 },
-                1904: { C: 1000 }
-            },
-            OSS: {
-                1902: { C: 16, S: 0},
-                1904: { C: 20, S: 0}
-            },
-            EXP: {
-                1902: { C: 0, S: 5},
-                1904: { C: 1, S: 6}
-            }
-        },
-        BLA: {
-            POP: {
-                1902: { C: 30 },
-                1904: { C: 100 }
-            },
-            OSS: {
-                1902: { C: 1, S: 5},
-                1904: { C: 20, S: 10}
-            },
-            EXP: {
-                1902: { C: 0, S: 5},
-                1904: { C: 0, S: 5}
-            }
-        },
-        WHI: {
-            POP: {
-                1902: { C: 500 },
-                1904: { C: 800 }
-            },
-            OSS: {
-                1902: { C: 1, S: 0},
-                1904: { C: 1, S: 0}
-            },
-            EXP: {
-                1902: { C: 0, S: 5},
-                1904: { C: 0, S: 5}
-            }
-        }
+    1902: {
+        C: 7,
+        P: 43,
+        S: 5,
+        aC: 46,
+        aP: 622
+    },
+    1904: {
+        C: 1,
+        P: 100,
+        S: 10,
+        aC: 46,
+        aP: 622
+    },
+    1905: {
+        C: 7,
+        P: 43,
+        S: 0,
+        aC: 46,
+        aP: 622
     }
 };
 
@@ -57,12 +34,13 @@ beforeEach(() => {
     L = require('leaflet-headless');
     Pattern = require('./leaflet.pattern.js');
     $ = require('jquery');
-    document.body.innerHTML = '<div class="viewport" id="leMap"><div id="map" class="map"></div>'
+    document.body.innerHTML ='<div class="viewport" id="leMap"><div id="map" class="map"></div><div class="year_selector" id="searchbox"></div>'
     M = require('./index.js');
 });
 
 test('style of value 5 returns white style', () => {
     M.processedData = mockData;
+    debugger;
     style = M.getOptions().style(mockFeature(1902));
     expect(style.fillColor).toEqual('white');
     expect(style.weight).toBe(1);
@@ -74,9 +52,8 @@ test('style of value 5 returns white style', () => {
 
 test('style of value 0 returns dark purple style', () => {
     M.processedData = mockData;
-    M.population = 6; // white
-    style = M.getOptions().style(mockFeature(1902));
-    expect(style.fillColor).toEqual('#54278f');
+    style = M.getOptions().style(mockFeature(1905));
+    expect(style.fillColor).toEqual('#2d004b');
     expect(style.weight).toBe(1);
     expect(style.opacity).toBe(1);
     expect(style.color).toBe('#b3b3b3');
@@ -86,9 +63,8 @@ test('style of value 0 returns dark purple style', () => {
 
 test('style of value 10 returns dark red style', () => {
     M.processedData = mockData;
-    M.population = 0; // black
     style = M.getOptions().style(mockFeature(1904));
-    expect(style.fillColor).toEqual('#a50f15');
+    expect(style.fillColor).toEqual('#aa0208');
     expect(style.weight).toBe(1);
     expect(style.opacity).toBe(1);
     expect(style.color).toBe('#b3b3b3');
